2664056ca53d31b41dcd52f487f0cd5f3d594175
xKeiro <nkevin@outlook.hu>
2022-10-15 12:22:20 +0200

    Code formatting and removing unnecessary imports

--- backend/routers/product_categories.py
+++ backend/routers/product_categories.py
@@ -1,27 +1,17 @@
-from datetime import timedelta
-
 from fastapi import APIRouter
 from fastapi import Depends
-from fastapi import HTTPException
-from fastapi import Request
-from fastapi import Response
-from fastapi import status
-from pydantic import EmailStr
 from pydantic import UUID4
 from sqlalchemy.orm import Session
-from sqlalchemy import or_
 
-from backend import oauth2
-from backend.oauth2 import AuthJWT
 from .. import models
 from .. import schemas
-from .. import util
 from ..config import settings
 from ..database import get_db
 
 router = APIRouter()
 ACCESS_TOKEN_EXPIRES_IN = settings.ACCESS_TOKEN_EXPIRES_IN
 REFRESH_TOKEN_EXPIRES_IN = settings.REFRESH_TOKEN_EXPIRES_IN
+
 
 @router.get('/', response_model=schemas.ProductsCategories)
 async def get_products_by_categories(db: Session = Depends(get_db)):
@@ -31,5 +21,6 @@ async def get_products_by_categories(db: Session = Depends(get_db)):
 
 @router.get('/{category_id}', response_model=schemas.ProductsResponse)
 async def get_products_by_category_id(category_id: UUID4, db: Session = Depends(get_db)):
-    products = db.query(models.Product).filter(models.Product.category_id == category_id, models.Product.discontinued == False).all()
+    products = db.query(models.Product).filter(models.Product.category_id == category_id,
+                                               models.Product.discontinued == False).all()
     return {"products": [product.__dict__ for product in products]}
