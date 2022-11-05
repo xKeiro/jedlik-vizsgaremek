dbe212d7891e9088745280384002aae2595ffaf1
xKeiro <nkevin@outlook.hu>
2022-11-05 15:08:34 +0100

    updated the database model for IDs to be server side

--- backend/models.py
+++ backend/models.py
@@ -19,7 +19,7 @@ from .database import Base
 class Address(Base):
     __tablename__ = 'address'
     id = Column(UUID(as_uuid=True), primary_key=True, nullable=False,
-                default=uuid.uuid4)
+                server_default=text("uuid_generate_v4()"))
     address = Column(String(50), nullable=False)
     city = Column(String(50), nullable=False)
     region = Column(String(50), nullable=False)
@@ -30,12 +30,12 @@ class Address(Base):
 class User(Base):
     __tablename__ = 'user'
     id = Column(UUID(as_uuid=True), primary_key=True, nullable=False,
-                default=uuid.uuid4)
+                server_default=text("uuid_generate_v4()"))
     username = Column(String(25), unique=True, nullable=False)
     first_name = Column(String(25), nullable=False)
     last_name = Column(String(25), nullable=False)
     email = Column(String(100), unique=True, nullable=False)
-    password = Column(String(50), nullable=False)
+    password = Column(String(60), nullable=False)
     phone = Column(String(20), nullable=True)
     address_id = Column(UUID(as_uuid=True), ForeignKey('address.id', ondelete='CASCADE'), nullable=False)
     photo = Column(String(250), nullable=True)
@@ -48,7 +48,7 @@ class User(Base):
 class Supplier(Base):
     __tablename__ = 'supplier'
     id = Column(UUID(as_uuid=True), primary_key=True, nullable=False,
-                default=uuid.uuid4)
+                server_default=text("uuid_generate_v4()"))
     company_name = Column(String(75), nullable=False)
     contact_first_name = Column(String(25), nullable=True)
     contact_last_name = Column(String(25), nullable=True)
@@ -61,7 +61,7 @@ class Supplier(Base):
 class ProductCategory(Base):
     __tablename__ = 'product_category'
     id = Column(UUID(as_uuid=True), primary_key=True, nullable=False,
-                default=uuid.uuid4)
+                server_default=text("uuid_generate_v4()"))
     title = Column(String(150), nullable=False)
     description = Column(String, nullable=False)
     photo = Column(String(250), nullable=True)
@@ -70,7 +70,7 @@ class ProductCategory(Base):
 class Product(Base):
     __tablename__ = 'product'
     id = Column(UUID(as_uuid=True), primary_key=True, nullable=False,
-                default=uuid.uuid4)
+                server_default=text("uuid_generate_v4()"))
     category_id = Column(UUID(as_uuid=True), ForeignKey('product_category.id'), nullable=False)
     sale_price = Column(Numeric(12, 2), nullable=False)
     title = Column(String(150), nullable=False)
@@ -85,7 +85,7 @@ class Product(Base):
 class ProductSupplier(Base):
     __tablename__ = 'product_supplier'
     id = Column(UUID(as_uuid=True), primary_key=True, nullable=False,
-                default=uuid.uuid4)
+                server_default=text("uuid_generate_v4()"))
     product_id = Column(UUID(as_uuid=True), ForeignKey('product.id'), nullable=False)
     supplier_id = Column(UUID(as_uuid=True), ForeignKey('supplier.id'), nullable=False)
     purchase_price = Column(Numeric(12, 2), nullable=False)
@@ -99,7 +99,7 @@ class ProductSupplier(Base):
 class Shipper(Base):
     __tablename__ = 'shipper'
     id = Column(UUID(as_uuid=True), primary_key=True, nullable=False,
-                default=uuid.uuid4)
+                server_default=text("uuid_generate_v4()"))
     phone = Column(String(20), nullable=False)
     email = Column(String(100), unique=True, nullable=False)
     price = Column(Numeric(12, 2), nullable=False)
@@ -108,7 +108,7 @@ class Shipper(Base):
 class Order(Base):
     __tablename__ = 'order'
     id = Column(UUID(as_uuid=True), primary_key=True, nullable=False,
-                default=uuid.uuid4)
+                server_default=text("uuid_generate_v4()"))
     user_id = Column(UUID(as_uuid=True), ForeignKey('user.id'), nullable=False)
     shipper_id = Column(UUID(as_uuid=True), ForeignKey('shipper.id'), nullable=False)
     vat = Column(Numeric(2, 2), nullable=False, server_default="0")
@@ -125,7 +125,7 @@ class Order(Base):
 class ProductOrder(Base):
     __tablename__ = 'product_order'
     id = Column(UUID(as_uuid=True), primary_key=True, nullable=False,
-                default=uuid.uuid4)
+                server_default=text("uuid_generate_v4()"))
     order_id = Column(UUID(as_uuid=True), ForeignKey('order.id'), nullable=False)
     product_id = Column(UUID(as_uuid=True), ForeignKey('product.id'), nullable=False)
     unit_price = Column(Numeric(12, 2), nullable=False)
@@ -141,7 +141,7 @@ class ProductOrder(Base):
 class ProductReview(Base):
     __tablename__ = 'product_review'
     id = Column(UUID(as_uuid=True), primary_key=True, nullable=False,
-                default=uuid.uuid4)
+                server_default=text("uuid_generate_v4()"))
     user_id = Column(UUID(as_uuid=True), ForeignKey('user.id'), nullable=False)
     product_id = Column(UUID(as_uuid=True), ForeignKey('product.id'), nullable=False)
     score = Column(Integer, nullable=False)
