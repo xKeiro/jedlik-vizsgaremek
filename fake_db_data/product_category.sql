4e159160cd232aa60f133a5806c688235cbfb51d
xKeiro <nkevin@outlook.hu>
2022-11-09 22:59:04 +0100

    Added initial data for product categories

---
+++ fake_db_data/product_category.sql
@@ -0,0 +1,10 @@
+INSERT INTO product_category (title, description) VALUES
+('Motherboards', 'A motherboard (also called mainboard, main circuit board, mb, mboard, backplane board, base board, system board, logic board (only in Apple computers) or mobo) is the main printed circuit board (PCB) in general-purpose computers and other expandable systems'),
+('Towers',  'A computer case, also known as a computer chassis, is the enclosure that contains most of the components of a personal computer (usually excluding the display, keyboard, and mouse)'),
+('Video Cards', 'A graphics card (also called a video card, display card, graphics adapter, GPU, VGA card/VGA, video adapter, or display adapter) is an expansion card which generates a feed of output images to a display device, such as a computer monitor.'),
+('Power Supplies', 'A power supply is an electrical device that supplies electric power to an electrical load. The main purpose of a power supply is to convert electric current from a source to the correct voltage, current, and frequency to power the load.'),
+('Memory','In computing, memory is a device or system that is used to store information for immediate use in a computer or related computer hardware and digital electronic devices.'),
+('Coolers','Computer cooling is required to remove the waste heat produced by computer components, to keep components within permissible operating temperature limits.'),
+('SSDs','A solid-state drive (SSD) is a solid-state storage device that uses integrated circuit assemblies to store data persistently, typically using flash memory, and functioning as secondary storage in the hierarchy of computer storage.'),
+('HDDs','A hard disk drive (HDD), hard disk, hard drive, or fixed disk is an electro-mechanical data storage device that stores and retrieves digital data using magnetic storage with one or more rigid rapidly rotating platters coated with magnetic material.'),
+('CPUs','A central processing unit (CPU), also called a central processor, main processor or just processor, is the electronic circuitry that executes instructions comprising a computer program.');
