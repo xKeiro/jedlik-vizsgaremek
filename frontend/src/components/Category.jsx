d7ab346bbce845bdbd173dd146909f4e373871ba
tercsd <tercsd@gmail.com>
2022-12-01 22:32:29 +0100

    finish ProductCard

--- frontend/src/components/Category.jsx
+++ frontend/src/components/Category.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import ProductCard from "./ProductCard";
 
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 import CircularProgress from "@mui/material/CircularProgress";
 
 export default function Category() {
@@ -39,8 +42,61 @@ export default function Category() {
     }
   }
 
+  function getMockProducts() {
+    const mockObj = {
+      products: [
+        {
+          id: "1",
+          sale_price: 10000,
+          title: "Sample Product 1",
+          description: "asd",
+          photo: "",
+          stock: 100,
+          discontinued: false,
+        },
+        {
+          id: "2",
+          sale_price: 100000,
+          title: "sample product 2",
+          description: "asd",
+          photo: "",
+          stock: 100,
+          discontinued: false,
+        },
+        {
+          id: "3",
+          sale_price: 1000000,
+          title: "sample product 3",
+          description: "asd",
+          photo: "",
+          stock: 100,
+          discontinued: false,
+        },
+        {
+          id: "4",
+          sale_price: 100,
+          title: "sample product 4",
+          description: "asd",
+          photo: "",
+          stock: 100,
+          discontinued: false,
+        },
+        {
+          id: "5",
+          sale_price: 480,
+          title: "outofstock product",
+          description: "asd",
+          photo: "",
+          stock: 0,
+          discontinued: false,
+        },
+      ],
+    };
+    setProducts(mockObj.products);
+  }
+
   useEffect(() => {
-    getCategoryProducts();
+    getMockProducts();
   }, []);
 
   return (
@@ -48,13 +104,34 @@ export default function Category() {
       <div>
         <h2>{products ? "Category Name" : "Category"}</h2>
       </div>
-      <div>
-        {products ? (
-          products.map((product) => <div>{product.title}</div>)
-        ) : (
-          <CircularProgress />
-        )}
-      </div>
+      <Box
+        className="Categories__Box"
+        sx={{
+          margin: "20px",
+          display: "flex",
+          flexWrap: "wrap",
+          flexDirection: "column",
+          alignContent: "center",
+        }}
+      >
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          spacing={2}
+        >
+          {products ? (
+            products.map((product) => (
+              <Grid item key={product.id} xs={12} md={4}>
+                <ProductCard product={product} />
+              </Grid>
+            ))
+          ) : (
+            <CircularProgress />
+          )}
+        </Grid>
+      </Box>
     </div>
   );
 }
--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link as RouterLink } from "react-router-dom";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -7,15 +8,15 @@ import { Button, CardActionArea, CardActions } from "@mui/material";
 
 export default function ProductCard({ product }) {
   return (
-    <Card key={product.id} sx={{ maxWidth: 345 }}>
+    <Card key={product.id} sx={{ maxWidth: 350 }}>
       <CardActionArea component={RouterLink} to={"/product/" + product.id}>
         <CardMedia
           component="img"
-          height="140"
+          height="150"
           image={
             product.photo
               ? product.photo
-              : "https://via.placeholder.com/150x150.png"
+              : "https://via.placeholder.com/350x150.png"
           }
           alt={product.title}
         />
@@ -24,12 +25,22 @@ export default function ProductCard({ product }) {
             {product.title}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            {product.sale_price}
+            {product.stock ? "In stock" : "Out of stock"}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {product.sale_price.toLocaleString("en-US", {
+              style: "currency",
+              currency: "HUF",
+            })}
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
+        <Button
+          size="small"
+          color="primary"
+          disabled={product.stock ? false : true}
+        >
           Add to cart
         </Button>
       </CardActions>
