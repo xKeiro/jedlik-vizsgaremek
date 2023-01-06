a90f2552432a7491d57aa39fab2ddb1adb3f50a4
xKeiro <nkevin@outlook.hu>
2023-01-06 13:05:29 +0100

    Added get user by id route

--- backend/routers/user.py
+++ backend/routers/user.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from fastapi import Depends
 from sqlalchemy.orm import Session
+from pydantic import UUID4
 
 from .. import models
 from .. import oauth2
@@ -15,3 +16,9 @@ def get_me(db: Session = Depends(get_db), user_id: str = Depends(oauth2.require_
     user = db.query(models.User).filter(models.User.id == user_id).first()
     address = db.query(models.Address).filter(models.User.id == user_id).first()
     return { "user": user, "address": address.__dict__ }
+
+@router.get('/{user_id}', response_model=schemas.UserResponse)
+async def get_products_by_category_id(user_id: UUID4, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    address = db.query(models.Address).filter(models.User.id == user_id).first()
+    return {"user": user, "address": address.__dict__}
