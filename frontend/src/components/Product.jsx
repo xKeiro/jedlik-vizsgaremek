c956712b9455a45cecded500a323561df46f58fc
tercsd <tercsd@gmail.com>
2023-01-11 18:44:54 +0100

    Add product page layout

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { Link as RouterLink } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActions } from "@mui/material";
 
 import CircularProgress from "@mui/material/CircularProgress";
 
@@ -45,7 +51,50 @@ export default function Product() {
       <div>
         <h2>{product ? product.title : "Product"}</h2>
       </div>
-      <div>{product ? <div>{product.title}</div> : <CircularProgress />}</div>
+      <div>
+        {product ? (
+          <Card key={product.id} sx={{}}>
+            <CardMedia
+              component="img"
+              height="500"
+              image={
+                product.photo
+                  ? product.photo
+                  : "https://via.placeholder.com/1920x1080.png"
+              }
+              alt={product.title}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {product.title}
+              </Typography>
+              <Typography gutterBottom variant="h6" component="div">
+                {product.description}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {product.stock ? "In stock" : "Out of stock"}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {product.sale_price.toLocaleString("en-US", {
+                  style: "currency",
+                  currency: "HUF",
+                })}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button
+                size="small"
+                color="primary"
+                disabled={product.stock ? false : true}
+              >
+                Add to cart
+              </Button>
+            </CardActions>
+          </Card>
+        ) : (
+          <CircularProgress />
+        )}
+      </div>
     </div>
   );
 }
