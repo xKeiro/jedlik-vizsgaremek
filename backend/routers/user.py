c6321fb43f1084c2e590946112515d435b656b2f
xKeiro <nkevin@outlook.hu>
2023-01-11 15:27:30 +0100

    Add a patch route for modifying user data

--- backend/routers/user.py
+++ backend/routers/user.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import status
 from sqlalchemy.orm import Session
 from pydantic import UUID4
 
@@ -7,6 +9,7 @@ from .. import models
 from .. import oauth2
 from .. import schemas
 from ..database import get_db
+from ..util import error_response_message
 
 router = APIRouter()
 
@@ -20,5 +23,35 @@ def get_me(db: Session = Depends(get_db), user_id: str = Depends(oauth2.require_
 @router.get('/{user_id}', response_model=schemas.UserResponse)
 async def get_products_by_category_id(user_id: UUID4, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=error_response_message("The user does not exists!"))
     address = db.query(models.Address).filter(models.User.id == user_id).first()
     return {"user": user, "address": address.__dict__}
+
+@router.patch('/{user_id}', response_model=schemas.UserResponse)
+async def get_products_by_category_id(user_id: UUID4, user: schemas.OptionalUserSchema | None, address: schemas.OptionalAddress | None, db: Session = Depends(get_db)):
+    user_query = db.query(models.User).filter(models.User.id == user_id)
+    if not user_query:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=error_response_message("The user does not exists!"))
+    # update_user = user_.dict(exclude_unset=True)
+    # updated_user = user.copy(update=update_user)
+    # user = updated_user
+    address_query = db.query(models.Address).filter(models.User.id == user_id)
+    if not address_query:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=error_response_message("The user does not have an address!"))
+    address_id = address_query.first().id
+    address_query = db.query(models.Address).filter(models.Address.id == address_id)
+    # update_address = address_.dict(exclude_unset=True)
+    user_query.update(user.dict(exclude_unset=True))
+    address_query.update(address.dict(exclude_unset=True))
+    # db.commit()
+    updated_user = user_query.first()
+    updated_address = address_query.first()
+    db.commit()
+    db.refresh(updated_user)
+    db.refresh(address_query)
+
+    return {"user": updated_user.__dict__, "address": updated_address.__dict__}
