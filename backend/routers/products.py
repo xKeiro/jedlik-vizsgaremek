7be0829663615b8a1d6dbcbdc5ebb64b83b9c14c
xKeiro <nkevin@outlook.hu>
2023-01-12 19:00:48 +0100

    Add post products route

--- backend/routers/products.py
+++ backend/routers/products.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from fastapi import HTTPException
+from fastapi import status
 from fastapi import Depends
 from pydantic import UUID4
 from sqlalchemy.orm import Session
@@ -6,6 +8,7 @@ from sqlalchemy.orm import Session
 from .. import models
 from ..schemas import product_schemas
 from ..database import get_db
+from ..util import error_response_message
 
 router = APIRouter()
 
@@ -14,6 +17,20 @@ router = APIRouter()
 async def get_products(db: Session = Depends(get_db)):
     products = db.query(models.Product).filter(models.Product.discontinued == False).all()
     return {"products": products}
+
+@router.post('/', response_model=product_schemas.ProductResponse)
+async def post_products(product: product_schemas.ProductInputPost, db: Session = Depends(get_db)):
+    category_exists = db.query(models.ProductCategory)\
+        .filter(models.ProductCategory.id == product.category_id)\
+        .first()
+    if not category_exists:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=error_response_message('Incorrect Category Id!'))
+    new_product = models.Product(**product.dict())
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+    return new_product
 
 @router.get('/featured', response_model=product_schemas.ProductsResponse)
 async def get_featured_products(db: Session = Depends(get_db)):
@@ -28,11 +45,6 @@ async def get_all_products(db: Session = Depends(get_db)):
     products = db.query(models.Product).all()
     return {"products": products}
 
-
-@router.post('/', response_model=product_schemas.ProductResponse)
-async def get_products(db: Session = Depends(get_db)):
-    products = db.query(models.Product).filter(models.Product.discontinued == False).all()
-    return {"products": products}
 
 @router.get('/{product_id}', response_model=product_schemas.ProductResponse)
 async def get_product_by_product_id(product_id: UUID4, db: Session = Depends(get_db)):
