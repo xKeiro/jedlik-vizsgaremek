fb1891af6c1e2869e26d22e6d552340253faca52
xKeiro <nkevin@outlook.hu>
2023-01-18 16:17:30 +0100

    Add schema for ShipperResponse

--- backend/schemas/shipper_schemas.py
+++ backend/schemas/shipper_schemas.py
@@ -5,3 +5,12 @@ from pydantic import FilePath
 from pydantic import UUID4
 from pydantic import constr
 from pydantic import StrictBool
+
+class ShipperResponse(BaseModel):
+    id: UUID4
+    company_name: constr(max_length = 75)
+    contact_first_name: constr(max_length = 25)
+    contact_last_name: constr(max_length = 25)
+    phone: constr(max_length = 20)
+    email: EmailStr
+    price: Decimal
