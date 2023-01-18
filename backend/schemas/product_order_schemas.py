45f24b5fc960cf1ec8a2320ad9c8079cac4d86ce
xKeiro <nkevin@outlook.hu>
2023-01-18 16:17:53 +0100

    Add schema for ProductOrderResponse

--- backend/schemas/product_order_schemas.py
+++ backend/schemas/product_order_schemas.py
@@ -5,3 +5,12 @@ from pydantic import FilePath
 from pydantic import UUID4
 from pydantic import constr
 from pydantic import StrictBool
+from .product_schemas import ProductResponse
+
+
+class ProductOrderResponse(BaseModel):
+    id: UUID4
+    product: ProductResponse
+    unit_price: Decimal
+    quantity: int
+    discount: Decimal
