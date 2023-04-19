42bb653f079069b616fd4e4dabdc37f837014f9b
xKeiro <nkevin@outlook.hu>
2023-04-19 19:46:40 +0200

    Backend: Add model for ProductReview

---
+++ backend/Models/Products/ProductReview.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models.Products
+{
+    public class ProductReview : BaseModel
+    {
+        [Required]
+        public required Product Product { get; set; }
+        [Required]
+        [Range(1, 5)]
+        public required byte Score { get; set; }
+        [Required]
+        [MinLength(3), MaxLength(3000)]
+        public required string Text { get; set; }
+    }
+}
