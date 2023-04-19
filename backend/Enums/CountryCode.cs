e2ddc50b8621c87ed7da0fa5ebaab3e87cc9109e
xKeiro <nkevin@outlook.hu>
2023-04-19 17:27:39 +0200

    Backend: Add CountryCode enum

---
+++ backend/Enums/CountryCode.cs
@@ -0,0 +1,33 @@
+﻿namespace backend.Enums
+{
+    public enum CountryCode
+    {
+        AT = 20, // Austria
+        BE = 21, // Belgium
+        BG = 20, // Bulgaria
+        HR = 25, // Croatia
+        CY = 19, // Cyprus
+        CZ = 21, // Czechia
+        DK = 25, // Denmark
+        EE = 20, // Estonia
+        FI = 24, // Finland
+        FR = 20, // France
+        DE = 19, // Germany
+        GR = 24, // Greece
+        HU = 27, // Hungary
+        IE = 23, // Ireland
+        IT = 22, // Italy
+        LV = 21, // Latvia
+        LT = 21, // Lithuania
+        LU = 17, // Luxembourg
+        MT = 18, // Malta
+        NL = 21, // Netherlands
+        PL = 23, // Poland
+        PT = 23, // Portugal
+        RO = 19, // Romania
+        SK = 20, // Slovakia
+        SI = 22, // Slovenia
+        ES = 21, // Spain
+        SE = 25  // Sweden
+    }
+}
