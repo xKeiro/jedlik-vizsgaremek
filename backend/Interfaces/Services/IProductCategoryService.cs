e3866b498c9eb1ba7374198458f4254f5bd4a707
xKeiro <nkevin@outlook.hu>
2023-04-19 23:17:59 +0200

    Backend: Add IProductCategoryService interface

---
+++ backend/Interfaces/Services/IProductCategoryService.cs
@@ -0,0 +1,12 @@
+﻿using backend.Dtos.Products.ProductCategories;
+using backend.Interfaces.Services.Bases;
+
+namespace backend.Interfaces.Services
+{
+    public interface IProductCategoryService :
+        IBasicCrudService<ProductCategoryPublic, ProductCategoryWithoutId>,
+        IUniqueService<ProductCategoryPublic, ProductCategoryWithoutId>
+    {
+
+    }
+}
