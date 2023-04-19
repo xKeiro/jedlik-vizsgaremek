afd69a742511502025c95c1fa4bbfe8ddf3fb82e
xKeiro <nkevin@outlook.hu>
2023-04-19 16:52:36 +0200

    Backend: Add helper class for getting environment variables

---
+++ backend/Utils/EnvironmentVariableHelper.cs
@@ -0,0 +1,42 @@
+﻿using System.Configuration;
+
+namespace backend.Utils;
+
+public static class EnvironmentVariableHelper
+{
+    public static string ConnectionString { get => GetDbConnectionString(); }
+    public static string JwtTokenKey { get => Get("JWT_TOKEN_KEY"); }
+    public static string FrontendUrl { get => Get("FRONTEND_URL"); }
+
+    private static string GetDbConnectionString()
+    {
+        var envKeyDbServer = "DB_SERVER";
+        var dbServer = Environment.GetEnvironmentVariable(envKeyDbServer);
+        ThrowErrorIfEnvironmentVariableNotExists(envKeyDbServer, dbServer);
+        var envKeyDbDatabase = "DB_DATABASE";
+        var dbDatabase = Environment.GetEnvironmentVariable(envKeyDbDatabase);
+        ThrowErrorIfEnvironmentVariableNotExists(envKeyDbDatabase, dbDatabase);
+        var envKeyDbId = "DB_ID";
+        var dbId = Environment.GetEnvironmentVariable(envKeyDbId);
+        ThrowErrorIfEnvironmentVariableNotExists(envKeyDbId, dbId);
+        var envKeyDbPassword = "DB_PASSWORD";
+        var dbPassword = Environment.GetEnvironmentVariable(envKeyDbPassword);
+        ThrowErrorIfEnvironmentVariableNotExists(envKeyDbPassword, dbPassword);
+        return $"Server={dbServer};Database={dbDatabase};User Id={dbId};Password={dbPassword};MultipleActiveResultSets=true;TrustServerCertificate=True";
+    }
+
+    private static string Get(string envKey)
+    {
+        var envVariable = Environment.GetEnvironmentVariable(envKey);
+        ThrowErrorIfEnvironmentVariableNotExists(envKey, envVariable);
+        return envVariable!;
+
+    }
+    private static void ThrowErrorIfEnvironmentVariableNotExists(string envKey, string? envValue)
+    {
+        if (string.IsNullOrEmpty(envValue))
+        {
+            throw new ConfigurationErrorsException($"Missing \"{envKey}\" environment variable!");
+        }
+    }
+}
