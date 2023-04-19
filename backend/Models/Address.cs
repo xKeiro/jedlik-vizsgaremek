0622f4a1c82688e0ea826127fe6c0fb826f1cad0
xKeiro <nkevin@outlook.hu>
2023-04-19 17:30:30 +0200

    Backend: Add model for Address

---
+++ backend/Models/Address.cs
@@ -0,0 +1,18 @@
+﻿using backend.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class Address : BaseModel
+    {
+        [MaxLength(50)]
+        public required string Street { get; set; }
+        [MaxLength(50)]
+        public required string City { get; set; }
+        [MaxLength(50)]
+        public required string Region { get; set; }
+        [MaxLength(10)]
+        public required string PostalCode { get; set; }
+        public required CountryCode Country { get; set; }
+    }
+}
