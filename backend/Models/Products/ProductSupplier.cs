9d5c5b611471316eccd9db0394bdb2cf88bb679c
xKeiro <nkevin@outlook.hu>
2023-04-19 18:21:43 +0200

    Backend: Add model for ProductSupplier

---
+++ backend/Models/Products/ProductSupplier.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models.Products
+{
+    public class ProductSupplier
+    {
+        [Required]
+        public required Product Product { get; set; }
+        [Required]
+        public required Supplier Supplier { get; set; }
+        [Required]
+        public required decimal PurchasePrice { get; set; }
+    }
+}
