b1e6ea606ff2537b5023ad87641cb80c6d6b2ba6
xKeiro <nkevin@outlook.hu>
2023-04-19 18:18:27 +0200

    Backend: Update existing model with required data annotation

--- backend/Models/Address.cs
+++ backend/Models/Address.cs
@@ -5,14 +5,19 @@ namespace backend.Models
 {
     public class Address : BaseModel
     {
+        [Required]
         [MinLength(3), MaxLength(50)]
         public required string Street { get; set; }
+        [Required]
         [MinLength(3), MaxLength(50)]
         public required string City { get; set; }
+        [Required]
         [MinLength(3), MaxLength(50)]
         public required string Region { get; set; }
+        [Required]
         [MaxLength(10)]
         public required string PostalCode { get; set; }
+        [Required]
         public required CountryCode Country { get; set; }
     }
 }
--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -4,18 +4,25 @@ namespace backend.Models
 {
     public class User: BaseModel
     {
+        [Required]
         [MinLength(3), MaxLength(25)]
         public required string Username { get; set; }
+        [Required]
         [MinLength(3), MaxLength(25)]
         public required string FirstName { get; set; }
+        [Required]
         [MinLength(3), MaxLength(25)]
         public required string LastName { get; set; }
+        [Required]
         [EmailAddress]
         public required string Email { get; set; }
+        [Required]
         [MinLength(60), MaxLength(60)]
         public required string Password { get; set; }
+        [Required]
         [Phone]
         public required string Phone { get; set; }
+        [Required]
         public required bool IsAdmin { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public required Address Address { get; set; }
