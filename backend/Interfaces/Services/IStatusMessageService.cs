c6c2d5331f11149d57c5c7ce7d55020ddc72baf6
xKeiro <nkevin@outlook.hu>
2023-04-19 23:18:20 +0200

    Backend: Add IStatusMessageService interface

---
+++ backend/Interfaces/Services/IStatusMessageService.cs
@@ -0,0 +1,13 @@
+﻿using backend.Models;
+
+namespace backend.Interfaces.Services;
+public interface IStatusMessageService
+{
+    StatusMessage AlreadyExists();
+    StatusMessage Deleted(ulong id);
+    StatusMessage NoneFound();
+    StatusMessage NotFound(ulong id);
+    StatusMessage NotUnique();
+    StatusMessage GenericError();
+    StatusMessage ANotExistingIdProvided();
+}
