e516c3fc085c3e6dc8c5eb79a0702a5a640d5152
xKeiro <nkevin@outlook.hu>
2023-04-19 18:05:47 +0200

    Backend: Add model for Supplier

---
+++ backend/Models/Supplier.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class Supplier: BaseModel
+    {
+        [MinLength(3), MaxLength(75)]
+        public required string CompanyName { get; set; }
+        [MinLength(3), MaxLength(25)]
+        public required string ContactFirstName { get; set; }
+        [MinLength(3), MaxLength(25)]
+        public required string ContactLastName { get; set; }
+        [Phone]
+        public required string Phone { get; set; }
+        [EmailAddress]
+        public required string Email { get; set; }
+        [Required]
+        public required Address Address { get; set; }
+    }
+}
