85ca573850743e26ab11179e50b562fa1c7baedc
xKeiro <nkevin@outlook.hu>
2023-04-19 17:28:22 +0200

    Backend: Add model for ProductCategory

---
+++ backend/Models/BaseModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class BaseModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public ulong Id { get; set; }
+    }
+}
---
+++ backend/Models/Product/ProductCategory.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models.Product
+{
+    public class ProductCategory: BaseModel
+    {
+        [MaxLength(150)]
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+    }
+}
