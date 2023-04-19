ed6ead36432fefa15c0f855cb925a7974fb7fec6
xKeiro <nkevin@outlook.hu>
2023-04-19 23:17:05 +0200

    Backend: Add IBasicCrudService interface

---
+++ backend/Interfaces/Services/Bases/IBasicCrudService.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Interfaces.Services.Bases
+{
+    public interface IBasicCrudService<TResponseDto, TRequestDto>
+        where TResponseDto : class
+        where TRequestDto : class
+    {
+        Task<List<TResponseDto>> GetAll();
+        Task<TResponseDto> Add(TRequestDto requestDto);
+        Task<TResponseDto?> Find(ulong id);
+        Task<TResponseDto> Update(ulong id, TRequestDto requestDto);
+    }
+}
