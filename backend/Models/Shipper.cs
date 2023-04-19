ca1c5790d7e2c043f0745d820967364b0d7c15a5
xKeiro <nkevin@outlook.hu>
2023-04-19 18:25:24 +0200

    Backend: Add model for Shipper

---
+++ backend/Models/Shipper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class Shipper: BaseModel
+    {
+        [Required]
+        [MinLength(3), MaxLength(75)]
+        public required string CompanyName { get; set; }
+        [Required]
+        [MinLength(3), MaxLength(25)]
+        public required string ContactFirstName { get; set; }
+        [Required]
+        [MinLength(3), MaxLength(25)]
+        public required string ContactLastName { get; set; }
+        [Required]
+        [Phone]
+        public required string Phone { get; set; }
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+        [Required]
+        [Precision(18, 2)]
+        public required decimal Price { get; set; }
+    }
+}
