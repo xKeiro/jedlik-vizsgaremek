cd552703ca2e1fe20b7d5a49c48fa46fea72f011
xKeiro <nkevin@outlook.hu>
2023-04-19 17:04:02 +0200

    Backend: Add base structure for db context

---
+++ backend/Services/JedlikContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace backend.Services
+{
+    public class JedlikContext : DbContext
+    {
+        public JedlikContext(DbContextOptions<JedlikContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Method intentionally left empty for future expansion.
+        }
+    }
+}
