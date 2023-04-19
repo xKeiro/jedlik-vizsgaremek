e2acc7b054958f6e0710b0b641700771b22f9818
xKeiro <nkevin@outlook.hu>
2023-04-19 19:18:38 +0200

    Backend: Add model for ProductOrder

---
+++ backend/Models/Orders/ProductOrder.cs
@@ -0,0 +1,22 @@
+﻿using backend.Models.Products;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models.Orders
+{
+    public class ProductOrder : BaseModel
+    {
+        [Required]
+        public required Product Product { get; set; }
+        [Required]
+        [Precision(18, 2)]
+        public required decimal BasePrice { get; set; }
+        [Required]
+        public required uint Quantity { get; set; }
+        [Required]
+        public required byte Discount { get; set; }
+        public decimal TotalPrice => BasePrice * Quantity * (Discount / 100);
+
+
+    }
+}
