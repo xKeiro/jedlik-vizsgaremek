c8af017c06f9b084923279c145b68c4ef40aae0c
xKeiro <nkevin@outlook.hu>
2023-04-19 16:46:19 +0200

    Backend: Add initial state for ASP.NET rewrite

--- fake_db_data/supplier.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO supplier (id, company_name, contact_first_name, contact_last_name, phone, email, address_id)
-VALUES ('5fe3c5c3-d3d6-41c1-a9b7-2a9ed66b4504', 'Acme Corp.', 'Jane', 'Doe', '+36301111114', 'supplier1@example.com', '2aee6c1f-774f-4cd9-8d62-4e139b5a9a7d');
-INSERT INTO supplier (id, company_name, contact_first_name, contact_last_name, phone, email, address_id)
-VALUES ('8c46da89-4af9-4875-872b-caf39b47a927', 'Gizmo Inc.', 'Mike', 'Williams', '+36301111115', 'supplier2@example.com', 'b998263b-53c7-43d8-9d39-80459c9d4897');
-INSERT INTO supplier (id, company_name, contact_first_name, contact_last_name, phone, email, address_id)
-VALUES ('df68254e-89b3-4b6e-8e12-91ffbdd3c201', 'Widgets R Us', 'Sara', 'Johnson', '+36301111116', 'supplier3@example.com', '9b817ea9-51a7-4f1c-9bff-0bbd51750e0b');
