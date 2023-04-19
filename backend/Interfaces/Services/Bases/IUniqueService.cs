32ace5f11bd477eb6bf61f9bfd84c194c3f92a1c
xKeiro <nkevin@outlook.hu>
2023-04-19 23:17:30 +0200

    Backend: Add IUniqueService interface

---
+++ backend/Interfaces/Services/Bases/IUniqueService.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Interfaces.Services.Bases
+{
+    public interface IUniqueService<TResponseDto, TRequestDto>
+        where TRequestDto : class
+        where TResponseDto : class
+    {
+        Task<bool> IsUnique(TResponseDto requestDto);
+        Task<bool> IsUnique(TRequestDto requestDto);
+    }
+}
