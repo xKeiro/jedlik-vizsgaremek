58f9d8e4ec1cc66d807eb27b66d6923ea851199a
xKeiro <nkevin@outlook.hu>
2023-04-20 04:21:36 +0200

    Backend: Add custom ApiConvention schema

---
+++ backend/Conventions/JedlikBasicApiConventions.cs
@@ -0,0 +1,44 @@
+﻿using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+namespace backend.Conventions;
+
+public static class JedlikBasicApiConventions<TResponseDto, TRequestDto> 
+    where TResponseDto : class
+    where TRequestDto : class
+{
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StatusMessage))]
+
+    public static void GetAll()
+    {
+        // Method intentionally left empty.
+    }
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StatusMessage))]
+    [ProducesResponseType(StatusCodes.Status409Conflict, Type = typeof(StatusMessage))]
+    public static void Add(
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)]
+        TRequestDto requestDto)
+    {
+        // Method intentionally left empty.
+    }
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StatusMessage))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(StatusMessage))]
+    public static void Get(ulong id)
+    {
+        // Method intentionally left empty.
+    }
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(StatusMessage))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(StatusMessage))]
+    [ProducesResponseType(StatusCodes.Status409Conflict, Type = typeof(StatusMessage))]
+    public static void Update(
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Any)]
+        TResponseDto responseDto)
+    {
+        // Method intentionally left empty.
+    }
+}
