6678f084ab2b32c59a3230dc342f904b111710b2
xKeiro <nkevin@outlook.hu>
2023-04-21 11:57:40 +0200

    Backend: Add InMemoryDatabaseHelper utility for tests

---
+++ backend-tests/Utils/InMemoryDatabaseHelper.cs
@@ -0,0 +1,18 @@
+﻿using backend.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_tests.Utils;
+
+public static class InMemoryDatabaseHelper
+{
+    public static JedlikContext GetInMemoryDatabaseContext(string databaseName)
+    {
+        var options = new DbContextOptionsBuilder<JedlikContext>()
+            .UseInMemoryDatabase(databaseName: databaseName)
+            .Options;
+        var context = new JedlikContext(options);
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+        return context;
+    }
+}
