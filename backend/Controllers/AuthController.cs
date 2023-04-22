3f7682da597cd001ea1c5521a73756af06b6e019
xKeiro <nkevin@outlook.hu>
2023-04-22 02:58:35 +0200

    Backend: Add AuthController with Register endpoint

---
+++ backend/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using backend.Dtos.Auth;
+using backend.Dtos.Users;
+using backend.Interfaces.Services;
+using backend.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Common;
+
+namespace backend.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ApiControllerBase
+{
+    private readonly IAuthService _service;
+
+    public AuthController(IAuthService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost("Register")]
+    public async Task<ActionResult<UserPublic>> Register(UserRegister registerDto)
+    {
+        var result = await _service.Register(registerDto);
+        if (result.IsT1)
+        {
+            return (ActionResult)Problem(result.AsT1);
+        }
+        var cookieOptions = new CookieOptions
+        {
+            HttpOnly = true,
+            SameSite = SameSiteMode.Strict,
+            Secure = true,
+            Expires = DateTime.Now.AddDays(EnvironmentVariableHelper.JwtTokenExpirationDay)
+        };
+        Response.Cookies.Append("token", result.AsT0.JwtToken, cookieOptions);
+        return Ok(result.AsT0.UserPublic);
+    }
+}
