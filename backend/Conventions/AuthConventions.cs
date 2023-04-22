528f245b40fe316d36643529e5c5c48878e53dff
xKeiro <nkevin@outlook.hu>
2023-04-22 16:51:44 +0200

    Backend: Fix Auth Convention

--- backend/Conventions/AuthConventions.cs
+++ backend/Conventions/AuthConventions.cs
@@ -6,7 +6,7 @@ namespace backend.Conventions;
 
 public static class AuthConventions
 {
-    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status409Conflict)]
@@ -15,7 +15,7 @@ public static class AuthConventions
     {
         // Method intentionally left empty.
     }
-    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
@@ -23,19 +23,11 @@ public static class AuthConventions
     {
         // Method intentionally left empty.
     }
-    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
     public static void Logout()
-    {
-        // Method intentionally left empty.
-    }
-    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
-    [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
-    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-    public static void RefreshToken()
     {
         // Method intentionally left empty.
     }
