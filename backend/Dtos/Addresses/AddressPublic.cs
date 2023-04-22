09fc8e5c111f3a0bf64c9c4fc9edfd52652a257e
xKeiro <nkevin@outlook.hu>
2023-04-22 02:54:21 +0200

    Backend: Add Address related DTOs

---
+++ backend/Dtos/Addresses/AddressPublic.cs
@@ -0,0 +1,24 @@
+﻿using backend.Enums;
+using backend.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Dtos.Addresses;
+
+public class AddressPublic
+{
+    [Required]
+    [MinLength(3), MaxLength(50)]
+    public required string Street { get; set; }
+    [Required]
+    [MinLength(3), MaxLength(50)]
+    public required string City { get; set; }
+    [Required]
+    [MinLength(3), MaxLength(50)]
+    public required string Region { get; set; }
+    [Required]
+    [MaxLength(10)]
+    public required string PostalCode { get; set; }
+    [Required]
+    [MinLength(3), MaxLength(100)]
+    public required string Country { get; set; }
+}
