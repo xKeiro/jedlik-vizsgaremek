82e0ffda973ef3c3e5fe76fbb33588686a217cec
xKeiro <nkevin@outlook.hu>
2023-04-22 02:54:39 +0200

    Backend: Add User related DTOs

---
+++ backend/Dtos/Users/UserPublic.cs
@@ -0,0 +1,30 @@
+﻿using backend.Dtos.Addresses;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.Dtos.Users;
+
+public class UserPublic
+{
+    public required ulong Id { get; set; }
+    [Required]
+    [MinLength(3), MaxLength(25)]
+    public required string Username { get; set; }
+    [Required]
+    [MinLength(3), MaxLength(25)]
+    public required string FirstName { get; set; }
+    [Required]
+    [MinLength(3), MaxLength(25)]
+    public required string LastName { get; set; }
+    [Required]
+    [EmailAddress]
+    [MinLength(5), MaxLength(100)]
+    public required string Email { get; set; }
+    [Required]
+    [Phone]
+    [MinLength(3), MaxLength(20)]
+    public required string Phone { get; set; }
+    [Required]
+    public required bool IsAdmin { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public required AddressPublic Address { get; set; }
+}
