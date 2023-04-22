7ab87a5e01f120fbf20291d19385e9d379797f64
xKeiro <nkevin@outlook.hu>
2023-04-22 02:55:01 +0200

    Backend: Add Auth related DTOs

---
+++ backend/Dtos/Auth/AuthResult.cs
@@ -0,0 +1,9 @@
+﻿using backend.Dtos.Users;
+
+namespace backend.Dtos.Auth;
+
+public class AuthResult
+{
+    public required string JwtToken { get; set; }
+    public required UserPublic UserPublic { get; set; }
+}
---
+++ backend/Dtos/Auth/UserLogin.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Dtos.Auth;
+
+public class UserLogin
+{
+    [Required]
+    [MinLength(5), MaxLength(100)]
+    public required string Identifier { get; set; }
+    [Required]
+    [MinLength(5), MaxLength(60)]
+    public required string Password { get; set; }
+}
