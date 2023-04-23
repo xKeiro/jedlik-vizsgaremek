b9e47e403c777219684ad7b94674a09c5fac6b82
xKeiro <nkevin@outlook.hu>
2023-04-23 17:17:21 +0200

    Backend: Add mapping for newly added DTOs

--- backend/Maps/MappingProfile.cs
+++ backend/Maps/MappingProfile.cs
@@ -1,10 +1,14 @@
 ﻿using AutoMapper;
 using backend.Dtos.Addresses;
 using backend.Dtos.Auth;
+using backend.Dtos.Orders;
+using backend.Dtos.Orders.OrderAddresses;
+using backend.Dtos.Orders.ProductOrders;
 using backend.Dtos.Products;
 using backend.Dtos.Products.ProductCategories;
 using backend.Dtos.Users;
 using backend.Models;
+using backend.Models.Orders;
 using backend.Models.Products;
 
 namespace backend.Maps;
@@ -26,5 +30,12 @@ public class MappingProfile : Profile
 
         _ = CreateMap<ProductRegister, Product>();
         _ = CreateMap<Product, ProductPublic>().ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.Category.Id));
+
+        _ = CreateMap<OrderRegister, Order>();
+        _ = CreateMap<Order, OrderPublic>();
+        _ = CreateMap<ProductOrderPublic, ProductOrder>();
+        _ = CreateMap<ProductOrder, ProductOrderPublic>();
+        _ = CreateMap<OrderAddressPublic, OrderAddress>();
+        _ = CreateMap<OrderAddress, OrderAddressPublic>();
     }
 }
