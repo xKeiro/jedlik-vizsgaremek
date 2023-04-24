0c111d8e31a151ea0240472605e213129a3745ed
xKeiro <nkevin@outlook.hu>
2023-04-25 00:01:22 +0200

    Backend: Update ProductReview so that a user only allowed to have one review per product

---
+++ backend/Migrations/20230424215706_OneUserShouldHaveOnlyOneReviewPerProduct.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class OneUserShouldHaveOnlyOneReviewPerProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ProductReviews_ProductId",
+                table: "ProductReviews");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductReviews_ProductId_UserId",
+                table: "ProductReviews",
+                columns: new[] { "ProductId", "UserId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ProductReviews_ProductId_UserId",
+                table: "ProductReviews");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductReviews_ProductId",
+                table: "ProductReviews",
+                column: "ProductId");
+        }
+    }
+}
