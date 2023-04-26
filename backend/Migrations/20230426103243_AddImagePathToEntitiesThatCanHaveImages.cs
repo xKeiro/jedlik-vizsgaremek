0b125586069bf936fcca6dfe06c0e6e577bb38cc
xKeiro <nkevin@outlook.hu>
2023-04-26 13:48:18 +0200

    Backend: Add image path to models that need them

---
+++ backend/Migrations/20230426103243_AddImagePathToEntitiesThatCanHaveImages.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImagePathToEntitiesThatCanHaveImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Users",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Products",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "ProductCategories",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "ProductCategories");
+        }
+    }
+}
