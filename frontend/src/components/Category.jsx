633cecd1dcca9fc1735b7bba879c16d52c4047c2
xKeiro <nkevin@outlook.hu>
2023-04-27 12:06:07 +0200

    Frontend: Add infinite scrolling to products filtered by categories

--- frontend/src/components/Category.jsx
+++ frontend/src/components/Category.jsx
@@ -8,88 +8,107 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import CircularProgress from "@mui/material/CircularProgress";
 import CategoryBar from "./Shared/CategoryBar";
+import InfiniteScroll from "react-infinite-scroller";
 
 export default function Category() {
   const { id } = useParams();
+  const pageSize = 6;
   const [products, setProducts] = useState(null);
+  const [nextPage, setNextPage] = useState(1);
 
-  useEffect(() => {
-    async function getCategoryProducts() {
-      if (!id) {
-        setProducts([]);
-        return;
-      }
-      try {
-        const response = await fetch(
-          process.env.REACT_APP_API + `/api/products/category/${id}`,
-          {
-            method: "GET",
-            mode: "cors",
-            headers: {
-              "Content-type": "application/json",
-            },
-            credentials: "include",
-          }
-        );
-        const responseBody = await response.json();
-        if (!response.ok) {
-          const errorMessage = responseBody.title;
-          console.log(errorMessage);
-          return;
-        }
-        setProducts(responseBody);
-      } catch (error) {
-        console.log(error);
-        return;
-      }
+  async function getCategoryProducts(pageToLoad) {
+    if (!id) {
+      setProducts([]);
+      return;
     }
-    getCategoryProducts();
+    try {
+      const response = await fetch(
+        process.env.REACT_APP_API +
+          `/api/products/category/${id}?page=${pageToLoad}&pageSize=${pageSize}`,
+        {
+          method: "GET",
+          mode: "cors",
+          headers: {
+            "Content-type": "application/json",
+          },
+          credentials: "include",
+        }
+      );
+      const responseBody = await response.json();
+      if (!response.ok) {
+        const errorMessage = responseBody.title;
+        console.log(errorMessage);
+        return;
+      } else {
+        if (products === null) {
+          setProducts(responseBody.products);
+        } else {
+          setProducts(products.concat(responseBody.products));
+        }
+        setNextPage(responseBody.nextPage);
+      }
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+  }
+  useEffect(() => {
+    setProducts(null);
+    getCategoryProducts(nextPage);
+    setNextPage(1);
   }, [id]);
 
   return (
     <div className="Category">
-      <Box
-        className="Category__Box"
-        sx={{
-          margin: "20px",
-          display: "flex",
-          flexWrap: "wrap",
-          flexDirection: "column",
-          alignContent: "center",
-        }}
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={() => getCategoryProducts(nextPage)}
+        hasMore={nextPage !== null}
+        loader={<CircularProgress key="loading" />}
       >
-        <Grid
-          container
-          direction="row-reverse"
-          justifyContent="center"
-          spacing={2}
-          sx={{ marginBottom: 3 }}
+        <Box
+          className="Category__Box"
+          sx={{
+            margin: "20px",
+            display: "flex",
+            flexWrap: "wrap",
+            flexDirection: "column",
+            alignContent: "center",
+          }}
         >
-          <Grid item xs={12} md={9}>
-            <Box>
-              <Paper elevation={2}>
-                <h3>Products in this category</h3>
-              </Paper>
-            </Box>
-            <Grid container direction="row" spacing={2}>
-              {products ? (
-                products.map((product) => (
-                  <Grid item key={product.id} xs={12} md={6}>
-                    <ProductCard product={product} />
-                  </Grid>
-                ))
-              ) : (
-                <CircularProgress />
-              )}
+          <Grid
+            container
+            direction="row-reverse"
+            justifyContent="center"
+            spacing={2}
+            sx={{ marginBottom: 3 }}
+          >
+            <Grid item xs={12} md={9}>
+              <Box>
+                <Paper elevation={2}>
+                  <h3>Products in this category</h3>
+                </Paper>
+              </Box>
+              <Grid container direction="row" spacing={2}>
+                {products ? (
+                  products.map((product) => (
+                    <Grid item key={product.id} xs={12} md={6}>
+                      <ProductCard product={product} />
+                    </Grid>
+                  ))
+                ) : (
+                  <div></div>
+                )}
+              </Grid>
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <CategoryBar
+                currentCategoryId={products ? products[0].categoryId : null}
+              />
             </Grid>
           </Grid>
-          <Grid item xs={12} md={3}>
-            <CategoryBar
-              currentCategoryId={products ? products[0].categoryId : null}
-            />
-          </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      </InfiniteScroll>
     </div>
   );
 }
